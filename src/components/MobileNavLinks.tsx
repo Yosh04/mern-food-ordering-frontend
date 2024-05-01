f9ec1efd1fefbc341197a4184b8b033ecaf2c05c
Yosh <antonyescor4@gmail.com>
2024-04-30 21:32:27 -0600

    fix the problem with the components not used

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
 import { useAuth0 } from '@auth0/auth0-react'
 
 function MobileNavLinks() {
-    const {isAuthenticated, logout} = useAuth0();
+    const {logout} = useAuth0();
 
   return (
     <>
